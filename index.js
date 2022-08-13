459ae22234734076898bbda139876559157af2c8
peter <+>
2022-08-13 17:31:39 -0400

    commit tree 2f6161b1b0445ab584c6356755c832565f80e73b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const thankNames = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(thankNames){
+    let thankedNames = [];
+    for (let i = 0; i < thankNames.length; i++){
+        thankedNames.push(`Thank you, ${thankNames[i]}, for the wonderful surprise gift!`);
+    }
+    return thankedNames;
+}
+
+const testNum = 11;
+function countDown(testNum){
+    for(let i = testNum; i >= 0 ; i--){
+    console.log(i);
+    }
+}
